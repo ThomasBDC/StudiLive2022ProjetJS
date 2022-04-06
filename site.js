296756a20ebc6846e035d818a41490579eeffafc
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-04-06 20:08:10 +0200

    add some events

--- site.js
+++ site.js
@@ -2,6 +2,12 @@ function RefreshWysiwyg(){
     let textTitre = document.getElementById('titreWysiwyg').value;
     document.querySelector('#titreWysiwygResultat').textContent = textTitre;
 
+    let colorTitle = document.getElementById("colorTitleWysiwyg").value;
+    document.getElementById('titreWysiwygResultat').style.color = colorTitle;
+
+    let sizeTitle = document.getElementById("tailleTitleWysiwyg").value;
+    document.getElementById('titreWysiwygResultat').style.fontSize = `${sizeTitle}px`; 
+
     let textContent = document.getElementById('contentWysiwyg').value;
     document.querySelector('#contentWysiwygResultat').textContent = textContent;
 
@@ -9,7 +15,21 @@ function RefreshWysiwyg(){
     document.getElementById('imgWysiwygResultat').src = imgSrc;
 
     let imageMaxWith = document.getElementById("imgMaxWidthWysiwyg").value;
-    //document.getElementById('imgWysiwygResultat').clientWidth = imageMaxWith;
+    document.getElementById('imgWysiwygResultat').style.width = ''+imageMaxWith+'px';
+}
+
+function OnMouseOver(){
+    var tousLabels = document.querySelectorAll("input");
+    tousLabels.forEach(monLabel => {
+        monLabel.style.padding = '5px';
+    });
+}
+
+function OnMouseLeave(){
+    var tousLabels = document.querySelectorAll("input");
+    tousLabels.forEach(monLabel => {
+        monLabel.style.padding = 'inherit';
+    });
 }
 
 var btn = document.getElementById("btnGenererWysiwyg");
@@ -20,4 +40,8 @@ mesInputsWithEvents.forEach(monInput => {
     //Mon itération
     monInput.addEventListener("keyup", RefreshWysiwyg);
     monInput.addEventListener("change", RefreshWysiwyg);
+
+    monInput.addEventListener("mouseover", OnMouseOver)
+    monInput.addEventListener("mouseleave", OnMouseLeave)
 });
+
