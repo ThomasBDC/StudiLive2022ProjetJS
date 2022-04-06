46d35a2e8e6891ee75ea99f646ea6ee7cda4dbc2
Thomas BUREAU DU COLOMBIER <sio.bureauducolombier@gmail.com>
2022-04-06 19:25:29 +0200

    Ajout de l'input de largeur max

--- site.js
+++ site.js
@@ -1,3 +1,19 @@
+function RefreshWysiwyg(){
+    let textTitre = document.getElementById('titreWysiwyg').value;
+    document.querySelector('#titreWysiwygResultat').textContent = textTitre;
+
+    let textContent = document.getElementById('contentWysiwyg').value;
+    document.querySelector('#contentWysiwygResultat').textContent = textContent;
+
+    let imgSrc= document.getElementById('imgWysiwyg').value;
+    document.getElementById('imgWysiwygResultat').src = imgSrc;
+
+    let imageMaxWith = document.getElementById("imgMaxWidthWysiwyg").value;
+    //document.getElementById('imgWysiwygResultat').clientWidth = imageMaxWith;
+}
+
+var btn = document.getElementById("btnGenererWysiwyg");
+btn.addEventListener('click', RefreshWysiwyg);
 var mesInputsWithEvents = document.querySelectorAll(".onChangeRefreshWysiwyg");
 
 mesInputsWithEvents.forEach(monInput => {
